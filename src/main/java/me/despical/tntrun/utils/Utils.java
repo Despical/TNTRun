8d3562d80d5b50ecf3c9034fafb782e32f82fb1a
Berke Akçen <berkeakcen@gmail.com>
2023-09-30 20:39:35 +0300

    Removed SignAPI usage

--- src/main/java/me/despical/tntrun/utils/Utils.java
+++ src/main/java/me/despical/tntrun/utils/Utils.java
@@ -1,11 +1,8 @@
 package me.despical.tntrun.utils;
 
-import de.rapha149.signgui.version.*;
 import me.despical.tntrun.Main;
 import me.despical.tntrun.arena.ArenaState;
 import me.despical.tntrun.user.User;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -58,38 +55,5 @@ public class Utils {
 			"■".repeat(Math.max(0, progressBars)) +
 			"§c" +
 			"■".repeat(Math.max(0, leftOver));
-	}
-
-	public @SuppressWarnings("unused") static void addItem(Player player, ItemStack itemStack, int itemPos) {
-		if (player == null) return;
-
-		var inv = player.getInventory();
-		var item = inv.getItem(itemPos);
-
-		if (item != null) {
-			item.setAmount(item.getAmount() + itemStack.getAmount());
-			return;
-		}
-
-		inv.setItem(itemPos, itemStack);
-	}
-
-	static {
-		@SuppressWarnings("unused") final Class<?>[] shadePluginHack = {
-			Wrapper1_20_R1.class,
-			Wrapper1_19_R3.class,
-			Wrapper1_19_R2.class,
-			Wrapper1_19_R1.class,
-			Wrapper1_18_R2.class,
-			Wrapper1_17_R1.class,
-			Wrapper1_16_R3.class,
-			Wrapper1_15_R1.class,
-			Wrapper1_14_R1.class,
-			Wrapper1_13_R2.class,
-			Wrapper1_12_R1.class,
-			Wrapper1_11_R1.class,
-			Wrapper1_10_R1.class,
-			Wrapper1_9_R2.class,
-		};
 	}
 }
